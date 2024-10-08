87b24fc2c0744328d188ccd35d4722b96105e33f
Trifon Livia Mihaela <livaa94@yahoo.com>
2024-10-08 09:11:06 +0300

    proiect

---
+++ retete.py
@@ -0,0 +1,32 @@
+#in acest modul voi depozita informatii pe care mai apoi le voi importa
+placinta_cu_mere={str, int}
+
+placinta_cu_mere={"unt":50 ,
+                  "mere":6,
+                  "zahar pudra":100,
+                  "scortisoara":1,
+                  "oua":6,
+                  "faina":180,
+                  "zahar":200}
+
+briose_cu_fructe={str,int}
+
+briose_cu_fructe={"banane":2,
+                  "iaurt grecesc":150,
+                  "oua":2,
+                  "faina":150,
+                  "praf copt":1,
+                  "ulei de cocos":1,
+                  "afine":20}
+
+tarta_cu_ciocolata={str,int}
+
+tarta_cu_ciocolata={"faina":250,
+                    "unt":200,
+                    "apa rece":50,
+                    "zahar":20,
+                    "ciocolata":250,
+                    "smantana":250,
+                    "sare":1}
+
+
