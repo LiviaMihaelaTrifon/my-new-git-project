92eae701f76f9a8193b67fffec781c138340c9b7
Trifon Livia Mihaela <livaa94@yahoo.com>
2024-11-21 17:46:32 +0200

    ad

--- aplicatie.py
+++ aplicatie.py
@@ -1,1 +1,12 @@
-import retete 
+# creaza un program de tip calculator, care sa retina retete de prajituri in format json,unde sa poti adauga retete noi, sa calculeze necesarul de materie prima si sa-mi rezulte un pdf ca si necesar.
+
+from abc import ABC
+
+class Retetar(ABC):
+    def __init__(self)->None:
+        self.retetar={}
+
+    def adauga_reteta_noua(self,ingredient:str,cantitate:int):
+        self.retetar[ingredient]=cantitate
+        
+        
