7d9e5b472301af2f46b191d30bedc072c4df6e17
Trifon Livia Mihaela <livaa94@yahoo.com>
2025-01-01 14:19:29 +0200

    ad

--- model.py/database.py
+++ model.py/database.py
@@ -1,8 +1,12 @@
 import pymongo
 from pymongo import MongoClient
 
+parola=None
+with open("parola_mongo.txt", "r") as file:
+    parola=file.read
+
 class Database:
-    def __init__(self, uri="mariustrifon", db_name="patiserie"):
+    def __init__(self, uri=f"mongodb+srv://mariustrifon:{parola}@cluster0.2nwkg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", db_name="patiserie"):
         self.client = MongoClient(uri)
         self.db = self.client[db_name]
 
@@ -16,3 +20,14 @@ class Database:
         return self.db.ingredients.find_one({"name": ingredient_name})
 
 
+# from pymongo.mongo_client import MongoClient
+# from pymongo.server_api import ServerApi
+# uri = "mongodb+srv://mariustrifon:Mariusikklivia94@cluster0.2nwkg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+# # Create a new client and connect to the server
+# client = MongoClient(uri, server_api=ServerApi('1'))
+# # Send a ping to confirm a successful connection
+# try:
+#     client.admin.command('ping')
+#     print("Pinged your deployment. You successfully connected to MongoDB!")
+# except Exception as e:
+#     print(e)
